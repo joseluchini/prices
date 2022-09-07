1f9e0bfacc60eaab448c96be5fa877f5bd0c0adb
Jose <jose.luchini@gmail.com>
2022-09-07 19:50:29 -0300

    First commit

---
+++ src/test/java/com/capitole/prices/PricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
